aa6dfb393da1c7489b1a06b52b7594165bd5e8dc
GaS006 <grandea@yandex.ru>
2017-06-04 14:52:22 +0300

    Линейное уравнение

---
+++ src/LN.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Дэн on 04.06.2017.
+ */
+public class LN {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 30;
+        int x;
+
+        switch (x = b / a) {
+        }
+
+            System.out.println(x);
+
+
+    }
+
+}
